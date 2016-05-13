cde4d44c30a625e1ee9143383435229fefa7f60c
JackRenDeveloper <jackren_developer@163.com>
2016-05-13 14:04:58 -0700

    JackRen

--- DefineCell/ViewController.m
+++ DefineCell/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "MGSwipeButton.h"
+#import "MGSwipeTableCell.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate> {
+    UITableView *_tableView;
+}
 
 @end
 
@@ -17,6 +21,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    [self createTableView];
+}
+
+- (void)createTableView {
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame)) style:UITableViewStylePlain];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    _tableView.showsHorizontalScrollIndicator = NO;
+    _tableView.showsVerticalScrollIndicator = NO;
+    _tableView.separatorStyle =  UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:_tableView];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifer = @"identifer";
+    MGSwipeTableCell *cell = [tableView dequeueReusableCellWithIdentifier:identifer];
+    if ( cell == nil) {
+        cell = [[MGSwipeTableCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:identifer];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    
+//    cell.delegate = self; //optional
+    
+    
+    //configure left buttons
+    cell.leftButtons = @[[MGSwipeButton buttonWithTitle:@"1" icon:[UIImage imageNamed:@"check.png"] backgroundColor:[UIColor greenColor]],
+                         [MGSwipeButton buttonWithTitle:@"2" icon:[UIImage imageNamed:@"fav.png"] backgroundColor:[UIColor blueColor]]];
+    cell.leftSwipeSettings.transition = MGSwipeTransition3D;
+    
+    //configure right buttons
+    cell.rightButtons = @[[MGSwipeButton buttonWithTitle:@"Delete" backgroundColor:[UIColor redColor]],
+                          [MGSwipeButton buttonWithTitle:@"More" backgroundColor:[UIColor lightGrayColor]]];
+    cell.rightSwipeSettings.transition = MGSwipeTransition3D;
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
